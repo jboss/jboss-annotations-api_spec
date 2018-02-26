e2e05133f012a508648131518cbc3bb334749ae7
Tomaz Cerar <tomaz.cerar@gmail.com>
2018-02-26 09:58:16 +0100

    JBEE-186 jboss-annotations-api_1.3_spec-1.0.0.Final is missing Repeatble on javax.annotation.Resource

--- src/main/java/javax/annotation/Resource.java
+++ src/main/java/javax/annotation/Resource.java
@@ -1,29 +1,42 @@
 /*
- * The contents of this file are subject to the terms 
- * of the Common Development and Distribution License 
- * (the "License").  You may not use this file except 
- * in compliance with the License.
- * 
- * You can obtain a copy of the license at 
- * glassfish/bootstrap/legal/CDDLv1.0.txt or 
- * https://glassfish.dev.java.net/public/CDDLv1.0.html. 
- * See the License for the specific language governing 
- * permissions and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL 
- * HEADER in each file and include the License file at 
- * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable, 
- * add the following below this CDDL HEADER, with the 
- * fields enclosed by brackets "[]" replaced with your 
- * own identifying information: Portions Copyright [yyyy] 
- * [name of copyright owner]
- */
-
-/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2005-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://oss.oracle.com/licenses/CDDL+GPL-1.1
+ * or LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
-
 
 package javax.annotation;
 
@@ -32,7 +45,7 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
 
 /**
- * The Resource annotation marks a resource that is needed
+ * The <code>Resource</code> annotation marks a resource that is needed
  * by the application.  This annotation may be applied to an
  * application component class, or to fields or methods of the
  * component class.  When the annotation is applied to a
@@ -40,20 +53,21 @@ import static java.lang.annotation.RetentionPolicy.*;
  * of the requested resource into the application component
  * when the component is initialized.  If the annotation is
  * applied to the component class, the annotation declares a
- * resource that the application will look up at runtime. <p>
- *
- * Even though this annotation is not marked Inherited, deployment
+ * resource that the application will look up at runtime. 
+ * <p>
+ * Even though this annotation is not marked <code>Inherited</code>, deployment
  * tools are required to examine all superclasses of any component
  * class to discover all uses of this annotation in all superclasses.
  * All such annotation instances specify resources that are needed
  * by the application component.  Note that this annotation may
  * appear on private fields and methods of superclasses; the container
- * is required to perform injection in these cases as well.
+ * is required to perform injection in these cases as well.</p>
  *
- * @since Common Annotations 1.0
+ * @since 1.6, Common Annotations 1.0
  */
 @Target({TYPE, FIELD, METHOD})
 @Retention(RUNTIME)
+@Repeatable(Resources.class)
 public @interface Resource {
     /**
      * The JNDI name of the resource.  For field annotations,
@@ -67,6 +81,8 @@ public @interface Resource {
     /**
      * The name of the resource that the reference points to. It can
      * link to any compatible resource using the global JNDI names.
+     *
+     * @since 1.7, Common Annotations 1.1
      */
 
     String lookup() default "";
@@ -106,19 +122,14 @@ public @interface Resource {
     boolean shareable() default true;
 
     /**
-     * A product specific name that this resource should be mapped to.
-     * The name of this resource, as defined by the <code>name</code>
-     * element or defaulted, is a name that is local to the application
-     * component using the resource.  (It's a name in the JNDI
-     * <code>java:comp/env</code> namespace.)  Many application servers
-     * provide a way to map these local names to names of resources
-     * known to the application server.  This mapped name is often a
-     * <i>global</i> JNDI name, but may be a name of any form. <p>
-     *
-     * Application servers are not required to support any particular
+     * A product-specific name that this resource should be mapped to.
+     * The <code>mappedName</code> element provides for mapping the
+     * resource reference to the name of a resource known to the
+     * applicaiton server.  The mapped name could be of any form.
+     * <p>Application servers are not required to support any particular
      * form or type of mapped name, nor the ability to use mapped names.
      * The mapped name is product-dependent and often installation-dependent.
-     * No use of a mapped name is portable.
+     * No use of a mapped name is portable.</p>
      */
     String mappedName() default "";
 
